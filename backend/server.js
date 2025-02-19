81f80899a7445c8ef4578a2ff3699f723c590aff
Rocio Abigail Naal Mex <l21070051@valladolid.tecnm.mx>
2025-02-18 18:24:19 -0600

    definiendo arquitectura inicial de react en apss script

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const doGet = () =>
+  HtmlService.createTemplateFromFile("frontend/views/index")
+    .evaluate()
+    .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL)
+    .setSandboxMode(HtmlService.SandboxMode.IFRAME)
+    .addMetaTag(
+      "viewport",
+      'width=device-width,user-scalable=no,initial-scale=1,maximum-scale=1,minimum-scale=1"'
+    )
+    .setTitle("Proyecto apps script dev")
+    .setFaviconUrl("https://cdn-icons-png.flaticon.com/512/616/616490.png");
+
+const include = (ruta) =>
+  HtmlService.createHtmlOutputFromFile(ruta).getContent();
