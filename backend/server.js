18d9d87d049897b3139cc0889720412e9ee2867e
Rocio Abigail Naal Mex <l21070051@valladolid.tecnm.mx>
2025-02-18 22:51:17 -0600

    commit2

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const doGet = () =>
-  HtmlService.createTemplateFromFile("frontend/views/index")
+  HtmlService.createTemplateFromFile("frontend/index")
     .evaluate()
     .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL)
     .setSandboxMode(HtmlService.SandboxMode.IFRAME)
@@ -8,7 +8,7 @@ const doGet = () =>
       'width=device-width,user-scalable=no,initial-scale=1,maximum-scale=1,minimum-scale=1"'
     )
     .setTitle("Proyecto apps script dev")
-    .setFaviconUrl("https://cdn-icons-png.flaticon.com/512/616/616490.png");
+    .setFaviconUrl("https://image.flaticon.com/icons/png/512/953/953720.png");
 
 const include = (ruta) =>
   HtmlService.createHtmlOutputFromFile(ruta).getContent();
